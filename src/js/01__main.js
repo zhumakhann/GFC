11ec08c86e5d9b5a967db654f3ce097f044b62dd
Nurlan <nurlanzumahan@gmail.com>
2021-05-15 10:14:06 +0600

    feat: add popular

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-  new Splide( '.splide', {
+  new Splide( '#banners', {
     type   : 'loop',
     perPage: 1,
+    gap: 20,
+    arrows: false
   } ).mount();
+
+  new Splide( '#popular', {
+    perPage: 4,
+    gap: 10,
+    pagination: true,
+    arrows: false
+  } ).mount();
+  
   
 
 })
