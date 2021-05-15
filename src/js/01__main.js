b2b592e63ad656327aa45f7cb67bf4e7e1cf85b0
Nurlan <nurlanzumahan@gmail.com>
2021-05-15 15:22:05 +0600

    feat: add responsive for header

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -2,6 +2,12 @@ const header = document.querySelector('.header');
 const headerDropdownBtn = header.querySelector('.header__main-dropdown__selected-text')
 const headerDropdownContent = header.querySelector('.header__main-dropdown-content');
 const headerSearchBtn = header.querySelector('.header__nav-search__submit');
+const headerBurger = header.querySelector('.header__mobile-burger')
+const headerMobileNav = header.querySelector('.header__mobile-nav')
+const headerMobileNavClose = headerMobileNav.querySelector('.header__mobile-nav__top-close')
+const headerMobileSearch = header.querySelector('.header__mobile-search')
+const headerMobileSearchForm = header.querySelector('.header__mobile-form')
+
 
 function scrollHandler(){
   const fixedHeader = header.querySelector('.header__fixed')
@@ -17,7 +23,18 @@ document.addEventListener('DOMContentLoaded', () => {
   headerDropdownBtn.addEventListener('click', () => {
     headerDropdownContent.classList.toggle('active')
   })
+  headerBurger.addEventListener('click', () => {
+    headerMobileNav.classList.toggle('active')
+  })
 
+  headerMobileSearch.addEventListener('click', () => {
+    headerMobileSearchForm.classList.toggle('active')
+  })
+
+  headerMobileNavClose.addEventListener('click', () => {
+    headerMobileNav.classList.remove('active')
+
+  })
   headerSearchBtn.addEventListener('click', e => {
     e.preventDefault();
     const parent = e.target.closest('.header__nav-search');
