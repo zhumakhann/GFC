b701216e3f71711c2a6c534449c4128d89cad9fb
Nurlan <nurlanzumahan@gmail.com>
2021-05-16 11:33:31 +0600

    feat: add full responsive for index page

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -54,6 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
     gap: 20,
     arrows: false,
     pagination: false,
+    
 
   } ).mount();
 
@@ -61,7 +62,22 @@ document.addEventListener('DOMContentLoaded', () => {
     perPage: 4,
     gap: 10,
     pagination: true,
-    arrows: false
+    arrows: false,
+    breakpoints: {
+      600: {
+        perPage: 1,
+        pagination: false,
+      },
+      850: {
+        perPage: 2,
+        pagination: false,
+      },
+      1200: {
+        perPage: 3,
+        pagination: true
+
+      }
+    }
   } ).mount();
   
   
